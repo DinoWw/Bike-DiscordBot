f7d1f8f0096c0a31ff07e451f6bd01e69903ec8d
DinoWw <dinoplecko@yahoo.com>
2024-09-17 16:10:04 +0200

    sheets interface initial

--- commands/utility/bod.js
+++ commands/utility/bod.js
@@ -11,7 +11,7 @@ const botId = 1164580596006260737;
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('info')        // TODO: rename or make another command
+		.setName('bod')
 		.setDescription('Fetches relevant information!'),
 	async execute(interaction) {
       // await is needed so that future responses with followUp() do not raise errors
